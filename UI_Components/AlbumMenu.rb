dcd33850831962eeb671c3eb7a9a395bf5d50b41
GMKaitoKid <lynnguyen1311@gmail.com>
2023-11-14 19:54:04 +0700

    albums added

--- UI_Components/AlbumMenu.rb
+++ UI_Components/AlbumMenu.rb
@@ -12,19 +12,7 @@ class AlbumMenu
     @shape = Shape.new
     @drawing.addShape(shape)
   end
-  def playSelectedSongs
-    @buttons.each do |btn|
-      if(btn.selected)
-        @newTrack = btn.click()
-      end
-    end
-  end
-  def initmenu()
-    button_1 = PlayButton.new()
-    button_2 = PlayButton.new(0 , 100, 60, 60, Gosu::Color::GREEN, 'Ashes on The Fire.mp3')
-    @buttons << button_1
-    @buttons << button_2
-  end
+
   
 
 end
