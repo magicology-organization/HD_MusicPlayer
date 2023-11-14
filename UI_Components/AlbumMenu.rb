e8ac53e4751764605c95719f1b8c18bd7fb88af7
GMKaitoKid <lynnguyen1311@gmail.com>
2023-11-14 11:30:58 +0700

    PlayButton.rb added

--- UI_Components/AlbumMenu.rb
+++ UI_Components/AlbumMenu.rb
@@ -4,11 +4,27 @@ require './UI_Components/PlayButton.rb'
 class AlbumMenu
   attr_accessor :drawing
   def initialize(path)
-    @drawing = drawing.new
+    # @drawing = drawing.new
+    @buttons = Array.new()
+    
   end
   def addUIComponents
-    shape = Shape.new
+    @shape = Shape.new
     @drawing.addShape(shape)
   end
+  def playSelectedSongs
+    @buttons.each do |btn|
+      if(btn.selected)
+        @newTrack = btn.click()
+      end
+    end
+  end
+  def initmenu()
+    button_1 = PlayButton.new()
+    button_2 = PlayButton.new(0 , 100, 60, 60, Gosu::Color::GREEN, 'Ashes on The Fire.mp3')
+    @buttons << button_1
+    @buttons << button_2
+  end
+  
 
 end
--- UI_Components/PlayButton.rb
+++ UI_Components/PlayButton.rb
@@ -1,6 +1,16 @@
 require './Core_programs/Shape.rb'
-
+require 'gosu'
 class PlayButton < Shape
-
-
+    def initialize(x = 0, y = 0, width = 40, height = 40, color = Gosu::Color::RED, music_path = 'Akuma no Ko.mp3')
+        @x = x
+        @y = y  
+        @width = width
+        @height = height
+        @color = color
+        @selected = false 
+        @song_path = music_path
+    end
+    def emit_song()
+        return "./music/#{@song_path}"
+    end
 end
