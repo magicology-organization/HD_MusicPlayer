3b0fde7dfaee25c81f6b2a181c9061f28c579b07
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-11-15 21:55:34 +0700

    del + modified

--- program.rb
+++ program.rb
@@ -1,29 +1,27 @@
 require 'gosu'
-require './Core_programs/Drawing.rb'
-require './Core_programs/Shape.rb'
+require './UI_Components/MsMenu.rb'
 
-class MusicPlayer < Gosu::Window
-    def initialize
-        super(700, 700, false)
-        self.caption = "Music player"
-        @drawing = Drawing.new(Gosu::Color::WHITE)
+# module ZOrder
+#   TEXT = 1
+# end
+
+class TestWindow < Gosu::Window
+  def initialize()
+    super(800, 800, false)
+    self.caption = "Song Player"
+    # @font = Gosu::Font.new(80)
+    @menu = MsMenu.new("Classical")
+  end
+  def draw()
+    @menu.draw()
+    @menu.playSelectedSongs
+  end
+  def update()
+    if Gosu.button_down?(Gosu::MsLeft)
+      @menu.selected(mouse_x, mouse_y)
     end
-    def draw
-        @drawing.draw()
-    end
-    def update
-        if Gosu.button_down?(Gosu::MsRight)
-            shape = Shape.new(mouse_x, mouse_y, 10, 10, Gosu::Color::GREEN)
-            @drawing.addShape(shape)
-        end
-        if Gosu.button_down?(Gosu::MsLeft)
-            @drawing.select(mouse_x, mouse_y)
-        end
-    end
+  end
 end
 
-
-window = MusicPlayer.new()
-window.show
-
-
+test = TestWindow.new
+test.show
