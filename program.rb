c39a11d016a7f9135c9d832194eea42f06208397
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-11-21 22:52:51 +0700

    linked components

--- program.rb
+++ program.rb
@@ -1,5 +1,6 @@
 require 'gosu'
 require './UI_Components/MsMenu.rb'
+require './UI_Components/AlbumMenu.rb'
 
 # module ZOrder
 #   TEXT = 1
@@ -10,15 +11,25 @@ class TestWindow < Gosu::Window
     super(800, 800, false)
     self.caption = "Song Player"
     # @font = Gosu::Font.new(80)
-    @menu = MsMenu.new("AOT")
+    @albumMenu = AlbumMenu.new("./Albums")
+    @msMenu = MsMenu.new("./Albums/AOT")
+    @arrayMenu = Array.new([@albumMenu, @msMenu])
+    @albumMenu.functionButton.selected = true
   end
   def draw()
-    @menu.draw()
-    @menu.playSelectedSongs
+    @arrayMenu.each do |menu|
+      if(menu.functionButton.selected)
+        menu.draw()
+      end
+    end
   end
   def update()
-    if Gosu.button_down?(Gosu::MsLeft)
-      @menu.selected(mouse_x, mouse_y)
+    if (Gosu.button_down?(Gosu::MsLeft) && @albumMenu.functionButton.selected)
+      @albumMenu.functionButton.selected = !@albumMenu.functionButton.IsAt(mouse_x, mouse_y)
+      @msMenu.functionButton.selected = @msMenu.functionButton.IsAt(mouse_x, mouse_y)
+    elsif (Gosu.button_down?(Gosu::MsLeft) && @msMenu.functionButton.selected)
+      @albumMenu.functionButton.selected = @albumMenu.functionButton.IsAt(mouse_x, mouse_y)
+      @msMenu.functionButton.selected = !@msMenu.functionButton.IsAt(mouse_x, mouse_y)
     end
   end
 end
